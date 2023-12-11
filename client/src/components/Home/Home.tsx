4c07cd8920aa3ce003ade849965e14313033052b
coreyan1234 <coreyan1234@gmail.com>
2023-12-10 20:19:23 -0500

    Once the user authenticates spotify and is redirected, we are trying to get the token in the url

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavigationBar from "../NavigationBar";
 
+const clientId = "10d5ee494b4d47ada0ff6de74a7174fa";
+const redirectUri = 'http://localhost:3000/';
+
+// After the user accepts the authorization request, 
+// we can exchange the authorization code for an access token
+const getToken = async (code: string) => {
+    // stored in the previous step
+    let codeVerifier = localStorage.getItem('code_verifier');
+    console.log("Home getToken code verifier: ", codeVerifier);
+
+    const payload = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            client_id: clientId,
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: redirectUri,
+            code_verifier: codeVerifier!,
+        })
+    }
+
+    const api_token_url = "https://accounts.spotify.com/api/token";
+    const body = await fetch(api_token_url, payload);
+    const response = await body.json();
+
+    localStorage.setItem('access_token', response.access_token);
+}
+
+
 const Home = () => {
+    // Parse the URL to retrieve the authorization code from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    let code = urlParams.get('code');
+
+    // Checking if the authorization code exists in the URL
+    if (code) {
+        console.log("found code in URL")
+        getToken(code);
+    }
+    else {
+        console.log("no code in URL");
+    }
     return (
         <h1>Home placeholder</h1>
     )
